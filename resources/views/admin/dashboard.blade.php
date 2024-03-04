4171f6bcb8278a4ac0479417f5cbc6241539fb59
FedericoMorrillo <federicomorrillo98@gmail.com>
2024-03-04 17:26:23 +0100

    condizione modifica/crea ristorante

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -24,11 +24,20 @@
                 <p class="p-3 mb-4 text-white rounded">
                     Ora che hai eseguito l' accesso puoi gestire il il profilo del tuo ristorante al meglio aggiungendo i piatti che andranno ad arricchire il tuo menù visualizzato online
                 </p>
-                <!--creazione ristorante-->
+                
+                @if (Auth::check() && Auth::user()->restaurant)
+                <!-- Modifica ristorante -->
+                <button class="btn-org rounded p-2">
+                    <a class="text-dark" href="{{ route('admin.restaurants.edit', Auth::user()->restaurant->id) }}">Modifica ristorante</a>
+                </button>  
+            @else
+                <!-- Aggiungi ristorante -->
                 <button class="btn-org rounded p-2">
                     <a class="text-dark" href="{{ route('admin.restaurants.create') }}">Aggiungi ristorante</a>
-                </button>  
-                <!--/creazione ristorante-->
+                </button> 
+            @endif
+               
+                
             </div>        
             
         </div>
