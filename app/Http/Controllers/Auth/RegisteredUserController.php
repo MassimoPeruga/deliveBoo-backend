1e3167ddffd036a605839c80a82bbe2e1caf80e0
FedericoMorrillo <federicomorrillo98@gmail.com>
2024-03-05 12:45:12 +0100

    feat: monoform

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -31,10 +32,18 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
+            //utenti
             'name' => ['required', 'string', 'max:255'],
             'surname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            //ristoranti
+            'restaurant_name' => ['required', 'string'],
+            'address' => ['required', 'string'],
+            'phone' => ['required', 'string'],
+            'vat' => ['required', 'min:11', 'max:11', 'string', 'unique:restaurants,vat'],
+            'description' => ['string', 'max:500', 'nullable'],
+            'image' => ['nullable', 'image', 'max:4096'],
         ]);
 
         $user = User::create([
@@ -43,6 +52,17 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $restaurant = new Restaurant();
+        $restaurant->name = $request->restaurant_name;
+        $restaurant->address = $request->address;
+        $restaurant->phone = $request->phone;
+        $restaurant->image = $request->image;
+        $restaurant->description = $request->description;
+        $restaurant->vat = $request->vat;
+        $restaurant->user_id = $user->id;
+
+        $restaurant->save();
 
         event(new Registered($user));
 
