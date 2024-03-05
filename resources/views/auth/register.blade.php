a7557e1feb3f73ada38952da756867c366e99858
FedericoMorrillo <federicomorrillo98@gmail.com>
2024-03-05 12:45:12 +0100

    feat: monoform

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="mb-4 row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="mb-4 row">
-                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
+                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Cognome') }}</label>
 
                             <div class="col-md-6">
                                 <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="mb-4 row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -68,11 +68,59 @@
                         </div>
 
                         <div class="mb-4 row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
+                        </div>
+                        <!--ristoranti-->
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nome Ristorante*:</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" name="restaurant_name" required
+                                value="{{ old('name') }}">
+                            @error('name')
+                                <div class="alert alert-danger mt-3">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="address" class="form-label" name="address">Indirizzo*:</label>
+                            <input type="text" class="form-control" id="address" aria-describedby="emailHelp"name="address"
+                                required value="{{ old('address') }}">
+                            @error('address')
+                                <div class="alert alert-danger mt-3">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="phone" class="form-label">Numero di Telefono*:</label>
+                            <input type="text" class="form-control" id="phone" aria-describedby="emailHelp" name="phone"
+                                required value="{{ old('phone') }}">
+                            @error('phone')
+                                <div class="alert alert-danger mt-3">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="vat" class="form-label" name="vat">P.IVA*:</label>
+                            <input type="number" class="form-control" id="vat" aria-describedby="emailHelp"name="vat" required
+                                value="{{ old('vat') }}">
+                            @error('vat')
+                                <div class="alert alert-danger mt-3">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label" name="description">Descrizione:</label>
+                            <textarea class="form-control" id="description" rows="3" name="description">{{ old('description') }}</textarea>
+                            @error('description')
+                                <div class="alert alert-danger mt-3">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="image" class="form-label"name="image">Immagine</label>
+                            <input type="file" class="form-control" id="image" aria-describedby="emailHelp"name="image"
+                                value="{{ old('image') }}">
+                            @error('image')
+                                <div class="alert alert-danger mt-3">{{ $message }}</div>
+                            @enderror
                         </div>
 
                         <div class="mb-4 row mb-0">
@@ -82,6 +130,7 @@
                                 </button>
                             </div>
                         </div>
+                        
                     </form>
                 </div>
             </div>
