84c4cccc4db02662fe39076e4deeb4225a3364e2
cosimogiove <cosimo18497@gmail.com>
2024-03-05 10:53:17 +0100

    feat:stylebutton

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -5,26 +5,31 @@
         <h2 class="py-3 px-3">{{ $restaurant->name }}</h2>
     </div>
     <div class="d-flex justify-content-center">
-            <h2>Menù</h2>      
-        </div>
+        <h2>Menù</h2>
+    </div>
     <div class="container">
 
-        <ul class="list-unstyled">
+        <ul class="list-unstyled carddish">
             @foreach ($restaurant->dishes as $dish)
                 <li class="d-flex gap-5 my-3">
                     <div>
-                        {{ $dish->name }}
-                        {{ $dish->price }} &euro;
+                        <div>nome piatto: {{ $dish->name }}</div>
+                        <div>
+                            prezzo: {{ $dish->price }} &euro;
+                        </div>
+                        <div>
+
+                            descrizione: {{ $dish->description }}
+                        </div>
                     </div>
                     <div>
-                        {{ $dish->description }}
+                        <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-org btn-sm">modifica piatto</a>
                     </div>
-                    <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-org btn-sm">modifica piatto</a>
                 </li>
             @endforeach
         </ul>
         <div class="my-3">
-            <a href="{{ route('admin.dishes.create', $restaurant->id) }}" class="btn btn-org">Aggiungi piatto</a>
+            <a href="{{ route('admin.dishes.create', $restaurant->id) }}" class="btn btn-org btndish">Aggiungi piatto</a>
             <a href="{{ route('admin.dashboard', $restaurant->id) }}" class="btn btn-secondary">Indietro</a>
         </div>
 
