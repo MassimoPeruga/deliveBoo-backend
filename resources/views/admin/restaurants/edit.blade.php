9fc8a5b9f015ac2466157840f9c20f420a25067f
MassimoPeruga <spamma-mutevole0p@icloud.com>
2024-03-05 03:16:49 +0100

    fix: client validation

--- resources/views/admin/restaurants/edit.blade.php
+++ resources/views/admin/restaurants/edit.blade.php
@@ -2,51 +2,54 @@
 
 @section('content')
     <div class="container">
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
         <h1>
-            modifica:
+            Modifica:
         </h1>
 
         <form action="{{ route('admin.restaurants.update', $restaurant->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label" name="name" value="{{ $restaurant->name }}">name:</label>
+                <label for="exampleInputEmail1" class="form-label">Nome*:</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="name"
+                    required value="{{ old('name', $restaurant['name']) }}">
+                @error('name')
+                    <div class="alert alert-danger mt-3">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Indirizzo*:</label>
                 <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                    name="name" value="{{ $restaurant->name }}">
+                    name="address" required value="{{ old('address', $restaurant['address']) }}">
+                @error('address')
+                    <div class="alert alert-danger mt-3">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label"
-                    name="address"value="{{ $restaurant->address }}">address:</label>
-                <input type="text" class="form-control" id="exampleInputEmail1"
-                    aria-describedby="emailHelp"name="address" value="{{ $restaurant->address }}">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label"name="phone"
-                    value="{{ $restaurant->phone }}">phone:</label>
+                <label for="exampleInputEmail1" class="form-label">Numero di Telefono*:</label>
                 <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                    name="phone"value="{{ $restaurant->phone }}">
+                    name="phone" required value="{{ old('phone', $restaurant['phone']) }}">
+                @error('phone')
+                    <div class="alert alert-danger mt-3">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label" name="vat"value="{{ $restaurant->vat }}">vat:</label>
-                <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"name="vat"
-                    value="{{ $restaurant->vat }}">
+                <label for="exampleInputEmail1" class="form-label">P.IVA*:</label>
+                <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                    name="vat" required value="{{ old('vat', $restaurant['vat']) }}">
+                @error('vat')
+                    <div class="alert alert-danger mt-3">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label"
-                    name="description"value="{{ $restaurant->description }}">description:</label>
-                <input type="text" class="form-control" id="exampleInputEmail1"
-                    aria-describedby="emailHelp"name="description" value="{{ $restaurant->description }}">
+                <label for="exampleInputEmail1" class="form-label">Descrizione:</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                    name="description" value="{{ old('description', $restaurant['description']) }}">
+                @error('description')
+                    <div class="alert alert-danger mt-3">{{ $message }}</div>
+                @enderror
             </div>
-            <button type="submit" class="btn btn-primary">modifica</button>
+            <button type="submit" class="btn btn-primary">Modifica</button>
         </form>
     </div>
 @endsection
