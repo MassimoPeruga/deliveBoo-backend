0e7949a752494ed4c4d006b40fa47f4900fae673
RiccardoImperato <riccardo.imperato@libero.it>
2024-03-06 10:41:13 +0100

    fix: dish view

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -5,14 +5,15 @@
         <!--card-->
         <div class="card mt-5">
             <div class="container py-4 px-4">
+                <!--dettagli piatto-->
                 <div class="d-flex justify-content-between">
-                    <!--dettagli piatto-->
                     <div class="dish-deatails-container">
                         <h2 class="mb-3">{{ $dish->name }}</h2>
                         <!--img-->
                         @if ($dish->image)
                             <div class="dish-img-container">
-                                <img src="{{ asset('storage/' . $dish->image) }}" alt="{{ $dish->name }}">
+                                <img src="{{ asset('storage/' . $dish->image) }}" alt="{{ $dish->name }}"
+                                    class="card-img-top">
                             </div>
                         @endif
                         <!--/img-->
@@ -20,8 +21,8 @@
                         <h4>Descrizione/Ingredienti</h4>
                         <p>{{ $dish->description }}</p>
                     </div>
-                    <!--/dettagli piatto-->
                 </div>
+                <!--/dettagli piatto-->
                 <div class="pt-3 text-center">
                     <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-secondary btn mx-2">Modifica</a>
                     <a role="button" href="{{ route('admin.restaurants.show', $restaurant) }}" class="btn btn-org">
