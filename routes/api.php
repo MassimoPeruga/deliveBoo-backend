1530f18fc8ec460461c636c3e7917850f1a08a59
cosimogiove <cosimo18497@gmail.com>
2024-03-07 11:50:03 +0100

    feat:route api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('restaurants', [RestaurantController::class, 'index']);
+Route::get('restaurants/{id}', [RestaurantController::class, 'show']);
+Route::get('types', [TypeController::class, 'index']);
