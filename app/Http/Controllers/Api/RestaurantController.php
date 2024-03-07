d7d0553a3e0856a1545c2f4c3d9c1db6450fc868
FedericoMorrillo <federicomorrillo98@gmail.com>
2024-03-07 19:29:00 +0100

    feat:fixv4

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -8,9 +8,24 @@ use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $restaurants = Restaurant::all();
+
+        $typeId = $request->input('type');
+
+
+        if (!$typeId) {
+            return response()->json([
+                'success' => false,
+                'message' => 'È necessario specificare un ID di tipo per filtrare i ristoranti.'
+            ], 400);
+        }
+
+
+        $restaurants = Restaurant::whereHas('types', function ($query) use ($typeId) {
+            $query->where('type_id', $typeId);
+        })->get();
+
         return response()->json([
             'success' => true,
             'results' => $restaurants
