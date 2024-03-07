ecfe663c00c924f03a3274fb322e95f39447f8bb
MassimoPeruga <spamma-mutevole0p@icloud.com>
2024-03-07 13:11:18 +0100

    fix: api types img

--- app/Http/Controllers/Api/TypeController.php
+++ app/Http/Controllers/Api/TypeController.php
@@ -10,7 +10,12 @@ class TypeController extends Controller
 {
     public function index()
     {
-        $types = Type::all();
+        $types = Type::all()->map(function ($type) {
+            // Aggiungi il percorso dell'immagine all'oggetto Type
+            $type->image_url = asset('/storage/' . $type->image); // Assicurati che il percorso sia corretto
+
+            return $type;
+        });
         return response()->json([
             'success' => true,
             'results' => $types
