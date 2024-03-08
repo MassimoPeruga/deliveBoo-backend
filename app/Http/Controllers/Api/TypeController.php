9b514dfd9c4d692f2a8c5ee98363b094cafaeee4
RuxandraRambet <ruxandraramb@gmail.com>
2024-03-08 13:37:11 +0100

    feat: fixes

--- app/Http/Controllers/Api/TypeController.php
+++ app/Http/Controllers/Api/TypeController.php
@@ -10,19 +10,19 @@ class TypeController extends Controller
 {
     public function index()
     {
-        request()->validate(['key' => ['nullable', 'string']]);
-        if (request()->key) {
-            $types = Type::where('name', 'LIKE', '%' . request()->key . '%')->get()->map(function ($type) {
-                $type->image = asset('/storage/types/' . $type->image);
-                return $type;
-            });
-        } else {
-            $types = Type::all()->map(function ($type) {
+        // request()->validate(['key' => ['nullable', 'string']]);
+        // if (request()->key) {
+        //     $types = Type::all()->map(function ($type) {
+        //         $type->image = asset('/storage/types/' . $type->image);
+        //         return $type;
+        //     });
+        // } else {
+        $types = Type::all()->map(function ($type) {
 
-                $type->image = asset('/storage/types/' . $type->image);
-                return $type;
-            });
-        }
+            $type->image = asset('/storage/types/' . $type->image);
+            return $type;
+        });
+        // }
         if ($types) {
             return response()->json([
                 'success' => true,
