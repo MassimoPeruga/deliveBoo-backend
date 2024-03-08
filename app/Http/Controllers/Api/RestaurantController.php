a0eebf538c985ad5864c5a63af9ce7523af602a1
FedericoMorrillo <federicomorrillo98@gmail.com>
2024-03-08 12:09:51 +0100

    feat: cambio rotta restaurant

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -8,23 +8,33 @@ use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
+    // public function index(Request $request)
+    // {
+
+    //     $typeId = $request->input('type');
+
+
+    //     if (!$typeId) {
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'È necessario specificare un ID di tipo per filtrare i ristoranti.'
+    //         ], 400);
+    //     }
+
+
+    //     $restaurants = Restaurant::whereHas('types', function ($query) use ($typeId) {
+    //         $query->where('type_id', $typeId);
+    //     })->get();
+
+    //     return response()->json([
+    //         'success' => true,
+    //         'results' => $restaurants
+    //     ]);
+    // }
+
     public function index(Request $request)
     {
-
-        $typeId = $request->input('type');
-
-
-        if (!$typeId) {
-            return response()->json([
-                'success' => false,
-                'message' => 'È necessario specificare un ID di tipo per filtrare i ristoranti.'
-            ], 400);
-        }
-
-
-        $restaurants = Restaurant::whereHas('types', function ($query) use ($typeId) {
-            $query->where('type_id', $typeId);
-        })->get();
+        $restaurants = Restaurant::all();
 
         return response()->json([
             'success' => true,
