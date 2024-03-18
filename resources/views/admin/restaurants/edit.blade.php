aaf9651592931aa4e5c5c7265d5c385bdfdaea41
MassimoPeruga <spamma-mutevole0p@icloud.com>
2024-03-18 16:05:09 +0100

    fix: responsive layout

--- resources/views/admin/restaurants/edit.blade.php
+++ resources/views/admin/restaurants/edit.blade.php
@@ -27,8 +27,8 @@
                     <div class="alert alert-danger mt-3">{{ $message }}</div>
                 @enderror
             </div>
-            <div class="d-flex">
-                <div class="mb-3 me-4">
+            <div class="row">
+                <div class="mb-3 col-12 col-md-4">
                     <label for="exampleInputEmail1" class="form-label">Numero di Telefono*:</label>
                     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
                         name="phone" required value="{{ old('phone', $restaurant['phone']) }}">
@@ -36,7 +36,7 @@
                         <div class="alert alert-danger mt-3">{{ $message }}</div>
                     @enderror
                 </div>
-                <div class="mb-3 mx-4">
+                <div class="mb-3 col-12 col-md-4">
                     <label for="exampleInputEmail1" class="form-label">P.IVA*:</label>
                     <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
                         name="vat" required value="{{ old('vat', $restaurant['vat']) }}">
@@ -44,7 +44,7 @@
                         <div class="alert alert-danger mt-3">{{ $message }}</div>
                     @enderror
                 </div>
-                <div class="mb-3 ms-4">
+                <div class="mb-3 col-12 col-md-4">
                     <label for="image" class="form-label"name="image">Immagine</label>
                     <input type="file" class="form-control" id="image" aria-describedby="emailHelp"name="image"
                         value="{{ old('image', $restaurant['image']) }}">
@@ -54,34 +54,23 @@
                 </div>
             </div>
 
-            <div class="mb-3 px-2 row row-cols-6">
+            <div class="mb-3 px-2 row">
                 <label class="form-label col-12 px-1 fw-bold">Tipologie*:</label>
-                {{-- @foreach ($types as $type)
-                    <div class="col form-check">
-                        <input class="form-check-input" type="checkbox" value="{{ $type->id }}"
-                            id="type-{{ $type->id }}" name="types[]"
-                            @if ($errors->any()) {{ in_array($type->id, old('types', [])) ? 'checked' : '' }}
-                                @else
-                                {{ $restaurant->types->contains($type->id) ? 'checked' : '' }} @endif>>
-                        <label class="form-check-label" for="type-{{ $type->id }}">
-                            {{ $type->name }}
-                        </label>
-                    </div>
-                @endforeach --}}
-
                 @foreach ($types as $type)
-                    <div class="form-check form-check-inline">
-                        @if ($errors->any())
-                            <input class="form-check-input" type="checkbox" value="{{ $type->id }}" name="types[]"
-                                id="type-{{ $type->id }}"
-                                {{ in_array($type->id, old('types', [])) ? 'checked' : '' }}>
-                            <label class="form-check-label" for="type-{{ $type->id }}">{{ $type->name }}</label>
-                        @else
-                            <input class="form-check-input" type="checkbox" value="{{ $type->id }}" name="types[]"
-                                id="type-{{ $type->id }}"
-                                {{ $restaurant->types->contains($type->id) ? 'checked' : '' }}>
-                            <label class="form-check-label" for="type-{{ $type->id }}">{{ $type->name }}</label>
-                        @endif
+                    <div class="col-6 col-md-4 col-lg-2">
+                        <div class="form-check form-check-inline">
+                            @if ($errors->any())
+                                <input class="form-check-input" type="checkbox" value="{{ $type->id }}" name="types[]"
+                                    id="type-{{ $type->id }}"
+                                    {{ in_array($type->id, old('types', [])) ? 'checked' : '' }}>
+                                <label class="form-check-label" for="type-{{ $type->id }}">{{ $type->name }}</label>
+                            @else
+                                <input class="form-check-input" type="checkbox" value="{{ $type->id }}" name="types[]"
+                                    id="type-{{ $type->id }}"
+                                    {{ $restaurant->types->contains($type->id) ? 'checked' : '' }}>
+                                <label class="form-check-label" for="type-{{ $type->id }}">{{ $type->name }}</label>
+                            @endif
+                        </div>
                     </div>
                 @endforeach
 
